36a4b8fbe964c8c4dd2bf00ddfd028e5d4695171
dmitriy.b <dmitriy.b@smplt.net>
2022-02-25 12:22:08 +0500

    Refactoring: Contract fixed

--- src/Service.Registration/Models/RegistrationTokenInfo.cs
+++ src/Service.Registration/Models/RegistrationTokenInfo.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Service.Registration.Models
 {
+	[DataContract]
 	public class RegistrationTokenInfo
 	{
+		[DataMember(Order = 1)]
 		public Guid? RegistrationUserId { get; set; }
-		
+
+		[DataMember(Order = 2)]
 		public DateTime RegistrationTokenExpires { get; set; }
 	}
 }
